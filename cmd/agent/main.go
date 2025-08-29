3aa145f3ce1dc57c1f7dbde87bd5e0cc23fc810b
Дмитрий Казаков <dmitryifile@yandex.ru>
2025-08-29 10:06:24 +0300

    Добавил заготовку для клиента

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"time"
+
+	"github.com/kazakovdmitriy/go-musthave-metrics/internal/agent"
+)
+
+func main() {
+	for {
+		metrics := agent.GetMetric()
+		metricsMap := agent.MetricsToMap(metrics)
+
+		agent.SendMetrics(metricsMap)
+
+		time.Sleep(2 * time.Second)
+	}
+}
---
+++ internal/agent/agent.go
@@ -0,0 +1,65 @@
+package agent
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+)
+
+func GetMetric() MemoryMetrics {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	return MemoryMetrics{
+		Alloc:         float64(m.Alloc),
+		BuckHashSys:   float64(m.BuckHashSys),
+		Frees:         float64(m.Frees),
+		GCCPUFraction: m.GCCPUFraction,
+		GCSys:         float64(m.GCSys),
+		HeapAlloc:     float64(m.HeapAlloc),
+		HeapIdle:      float64(m.HeapIdle),
+		HeapInuse:     float64(m.HeapInuse),
+		HeapObjects:   float64(m.HeapObjects),
+		HeapReleased:  float64(m.HeapReleased),
+		HeapSys:       float64(m.HeapSys),
+		LastGC:        float64(m.LastGC),
+		Lookups:       float64(m.Lookups),
+		MCacheInuse:   float64(m.MCacheInuse),
+		MCacheSys:     float64(m.MCacheSys),
+		MSpanInuse:    float64(m.MSpanInuse),
+		MSpanSys:      float64(m.MSpanSys),
+		Mallocs:       float64(m.Mallocs),
+		NextGC:        float64(m.NextGC),
+		NumForcedGC:   float64(m.NumForcedGC),
+		NumGC:         float64(m.NumGC),
+		OtherSys:      float64(m.OtherSys),
+		PauseTotalNs:  float64(m.PauseTotalNs),
+		StackInuse:    float64(m.StackInuse),
+		StackSys:      float64(m.StackSys),
+		Sys:           float64(m.Sys),
+		TotalAlloc:    float64(m.TotalAlloc),
+	}
+}
+
+func MetricsToMap(metrics MemoryMetrics) map[string]float64 {
+	result := make(map[string]float64)
+
+	v := reflect.ValueOf(metrics)
+	t := reflect.TypeOf(metrics)
+
+	for i := 0; i < v.NumField(); i++ {
+		field := t.Field(i)
+		value := v.Field(i).Float()
+		result[field.Name] = value
+	}
+
+	return result
+}
+
+func SendMetrics(metricsMap map[string]float64) {
+	for name, value := range metricsMap {
+		fmt.Printf("Sending %s: %f\n", name, value)
+		// sendToServer(name, value)
+	}
+	fmt.Println()
+}
