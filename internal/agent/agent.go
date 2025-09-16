f6860af784ccca6a884f488a41faeae633c4f15a
Dmitriy Kazakov <dmitriyfile@yandex.ru>
2025-09-16 13:11:08 +0300

    Избавился от рефлексии

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -2,27 +2,11 @@ package agent
 
 import (
 	"fmt"
-	"reflect"
 )
-
-func metricsToMap(metrics MemoryMetrics) map[string]float64 {
-	result := make(map[string]float64)
-
-	v := reflect.ValueOf(metrics)
-	t := reflect.TypeOf(metrics)
-
-	for i := 0; i < v.NumField(); i++ {
-		field := t.Field(i)
-		value := v.Field(i).Float()
-		result[field.Name] = value
-	}
-
-	return result
-}
 
 func SendMetrics(client *Client, metrics MemoryMetrics) ([]byte, error) {
 
-	metricsMap := metricsToMap(metrics)
+	metricsMap := metrics.ToMap()
 
 	for name, value := range metricsMap {
 		_, err := client.Post(fmt.Sprintf("/update/gauge/%s/%f", name, value), nil)
